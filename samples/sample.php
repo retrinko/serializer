120621beb72c1050103a27edbff98a7636dab0bd
Miguel <miguel.mailbox@gmail.com>
2016-06-19 18:36:41 +0200

    Add some echoing.

--- samples/sample.php
+++ samples/sample.php
@@ -7,11 +7,15 @@ use Retrinko\Serializer\Serializers\PhpSerializer;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $value = ['a', 'b', 'c'];
+printf('Original value: %s', PHP_EOL);
+var_dump($value);
 
 $serializer = SerializerFactory::bySerializedContentType(JsonSerializer::SERIALIZED_CONTENT_TYPE);
 $serializedValue = $serializer->serialize($value);
+printf('Serialized value using %s: %s', 'JsonSerializer', PHP_EOL);
 var_dump($serializedValue);
 
 $serializer = SerializerFactory::bySerializedContentType(PhpSerializer::SERIALIZED_CONTENT_TYPE);
 $serializedValue = $serializer->serialize($value);
+printf('Serialized value using %s: %s', 'PhpSerializer', PHP_EOL);
 var_dump($serializedValue);
